65af748981205000252a1aab6741ffcac97b328c
Harendra Kumar <harendra@composewell.com>
2026-04-21 16:00:17 +0530

    Add a nix flake file

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "streamly-fsevents";
+
+  inputs = {
+    basepkgs.url = "git+ssh://git@github.com/composewell/streamly-packages?rev=69728978adc44f53b3dd907acb2eb5bd2415fd60";
+    nixpkgs.follows = "basepkgs/nixpkgs";
+    nixpkgs-darwin.follows = "basepkgs/nixpkgs-darwin";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-darwin, basepkgs }:
+    basepkgs.nixpack.mkOutputs {
+      inherit nixpkgs nixpkgs-darwin basepkgs;
+      name = "streamly-fsevents";
+      sources = basepkgs.nixpack.lib.localSource "streamly-fsevents" ./.;
+      packages = basepkgs.nixpack.lib.devPackage "streamly-fsevents";
+      #sources = import ./sources.nix;
+      #packages = import ./packages.nix;
+    };
+}
