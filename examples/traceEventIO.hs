fd4bf2314604dbcc5cefa58e6849d2e88da9f522
Harendra Kumar <harendra@composewell.com>
2026-05-08 16:56:41 +0530

    Update the traceEventIO example

--- examples/traceEventIO.hs
+++ examples/traceEventIO.hs
@@ -2,12 +2,12 @@
 
 import Control.Monad.IO.Class (MonadIO(..))
 import Debug.Trace (traceEventIO)
-import Foreign.C.Types
-import System.Posix.Signals
+import Foreign.C.Types ( CUInt(..) )
+import GHC.Conc (myThreadId, labelThread)
+import System.Posix.Signals ( blockSignals, fullSignalSet )
 
 foreign import ccall unsafe "unistd.h sleep"
     c_sleep :: CUInt -> IO CUInt
-
 
 {-# INLINE withTracingFlow #-}
 withTracingFlow :: MonadIO m => String -> m a -> m a
@@ -17,6 +17,9 @@ withTracingFlow tag action = do
     liftIO $ traceEventIO ("END:" ++ tag)
     pure res
 
+emptyBlock :: IO ()
+emptyBlock = return ()
+
 sleepBlock :: IO ()
 sleepBlock = do
     -- So that signals do not interrupt the sleep
@@ -24,19 +27,19 @@ sleepBlock = do
     _ <- c_sleep 10
     return ()
 
-{-
-sleepWithEvents :: IO ()
-sleepWithEvents = do
-    traceEventIO "before sleep"
-    sleepBlock
-    traceEventIO "after sleep"
--}
-
-emptyBlock :: IO ()
-emptyBlock = return ()
+{-# INLINE printSumLoop #-}
+printSumLoop :: Int -> Int -> Int -> IO ()
+printSumLoop _ _ 0 = print "All Done!"
+printSumLoop chunksOf from times = do
+    withTracingFlow "SUM" $ print $ sum [from..(from + chunksOf)]
+    printSumLoop chunksOf (from + chunksOf) (times - 1)
 
 main :: IO ()
 main = do
+    tid <- myThreadId
+    labelThread tid "main-thread"
+
     withTracingFlow "EMPTY" emptyBlock
     withTracingFlow "SLEEP" sleepBlock
-    return ()
+    withTracingFlow "LOOP" $ do
+         printSumLoop 10000 1 100
